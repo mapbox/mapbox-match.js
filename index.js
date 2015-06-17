804935b171a377ff163409ff38f14a18861a6380
Arun Ganesh <arun.planemad@gmail.com>
2015-06-17 16:04:06 -0400

    Write mapmatching function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var xhr = require('corslite'),
+var xhr = require('xhr'),
     tidy = require('geojson-tidy');
 
 module.exports.mapmatch = mapmatch;
@@ -6,33 +6,51 @@ module.exports.mapmatch = mapmatch;
 
 // Public function
 
-function mapmatch(geojson) {
+function mapmatch(geojson, map) {
 
-    //    var layer = featureLayer || L.geoJson();
 
-    // Create a tidy geojson
-    var tidyGeojson = tidy.tidy(geojson, {
+    // First tidy the geojson
+    var matchedGeojson = JSON.parse(tidy.tidy(geojson, {
         "minimumDistance": 20
-    });
+    }));
 
+    var featureLayer = L.mapbox.featureLayer().addTo(map);
+
+    // Hit mapmatching API for every feature
     var mapMatchAPI = "https://api-directions-johan-matching.tilestream.net/v4/directions/matching/mapbox.driving.json";
     var xhrUrl = mapMatchAPI + "?" + L.mapbox.accessToken;
 
-    console.log(tidyGeojson);
+    function matchFeature(feature) {
 
-    //    for (var i = 0; i < tidyGeojson.features.length; i++) {
-    //
-    //        xhr(xhrUrl, function (err, response) {
-    //            if (err) return layer.fire('error', {
-    //                error: err
-    //            });
-    //            addData(layer, JSON.parse(response.responseText));
-    //            layer.fire('ready');
-    //        });
-    //
-    //    }
+        xhrOptions = {
+            json: "",
+            uri: xhrUrl,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        xhr(xhrOptions, function (err, response, body) {
+            if (err) {
+                console.log(err);
+            }
+
+            console.log(body);
+
+        });
+
+    }
+
+    for (var i = 0; i < matchedGeojson.features.length; i++) {
+
+        matchFeature(matchedGeojson.features[i]);
+
+    }
 
 }
+
+
+
 
 // Test map matching
 // curl -X POST -d @test/cross-country.json "https://api-directions-johan-matching.tilestream.net/v4/directions/matching/mapbox.driving.json?access_token=pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ" --header "Content-Type:application/json"
